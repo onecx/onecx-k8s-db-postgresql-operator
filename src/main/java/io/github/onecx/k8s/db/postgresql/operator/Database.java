8065dfac40cd49957081c9eeab8992d8b65fbc7a
Andrej Petras <andrej@lorislab.org>
2023-12-14 22:29:43 +0100

    feat: rename api

--- src/main/java/io/github/onecx/k8s/db/postgresql/operator/Database.java
+++ src/main/java/io/github/onecx/k8s/db/postgresql/operator/Database.java
@@ -5,7 +5,7 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Version;
 
-@Group("io.github.onecx.operator.db.postgresql")
+@Group("onecx.github.io")
 @Version("v1")
 public class Database extends CustomResource<DatabaseSpec, DatabaseStatus> implements Namespaced {
 }
