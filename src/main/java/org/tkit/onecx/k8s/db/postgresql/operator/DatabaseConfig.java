60d347b5fc051f6ccab9cdd77c3f6051b9c8d5d3
Andrej Petras <andrej@ajka-andrej.com>
2024-09-17 09:20:09 +0200

    chore(deps): update quarkus3 parent to 0.65.0 version (#49)

--- src/main/java/org/tkit/onecx/k8s/db/postgresql/operator/DatabaseConfig.java
+++ src/main/java/org/tkit/onecx/k8s/db/postgresql/operator/DatabaseConfig.java
@@ -1,12 +1,20 @@
 package org.tkit.onecx.k8s.db.postgresql.operator;
 
+import io.quarkus.runtime.annotations.ConfigDocFilename;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
+@ConfigDocFilename("onecx-k8s-db-postgresql-operator.adoc")
 @ConfigMapping(prefix = "onecx.k8s.db.postgresql.operator")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
 public interface DatabaseConfig {
 
+    /**
+     * Server host configuration.
+     */
     @WithName("host")
     @WithDefault("postgresql")
     String host();
